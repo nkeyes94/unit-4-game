17c252b9df7c6cf0b7e7e77cdbbf57ff0897ed7f
Nathan Keyes <n.keyes1994@gmail.com>
2019-06-03 22:19:04 -0400

    Buttons and displays functioning correctly

--- Crystal-Collector/logic.js
+++ Crystal-Collector/logic.js
@@ -1,10 +1,48 @@
+// Function to create the random target
 function randomTarget(){
-    return Math.floor(Math.random() * (19, 120)) + 1;
+    var tester = document.getElementById("tester");
+    var randomT = Math.floor(Math.random() * (120 - 19) + 19) + 1;
+    tester.replaceWith(randomT);
 }
-
+randomTarget();
+//Function to create the random guess
 function randomGuess(){
-    return Math.floor(Math.random() * (1, 12)) +1;
+    return Math.floor(Math.random() * (1, 12)) + 1;
 }
 
-console.log(randomTarget());
+var firstButton = document.getElementById("firstButton");
+firstButton = {value:randomGuess()};
+
+var secondButton = document.getElementById("secondButton");
+secondButton = {value:randomGuess()};
+
+var thirdButton = document.getElementById("thirdButton");
+thirdButton = {value:randomGuess()};
+
+var guessTotal = 0;
+var secondTester = document.getElementById("secondBox");
+
+function oneBtn(){
+    guessTotal = guessTotal + firstButton.value;
+    secondTester.innerHTML=guessTotal;
+    console.log(guessTotal);
+}
+
+function twoBtn(){
+    guessTotal = guessTotal + secondButton.value;
+    secondTester.innerHTML=guessTotal;
+    console.log(guessTotal);
+}
+
+function threeBtn(){
+    guessTotal = guessTotal + thirdButton.value;
+    secondTester.innerHTML=guessTotal;
+    console.log(guessTotal);
+}
+
+
+console.log(firstButton.value + " 1 btn value");
+console.log(secondButton.value + " 2 btn value");
+console.log(thirdButton.value + " 3 btn value");
+// console.log(randomTarget());
 console.log(randomGuess());
